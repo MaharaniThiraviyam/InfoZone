698b47d2f3ff3d9569c0773046beec54546f604a
T.MAHARANI <maharani1118@gmail.com>
2018-10-20 14:49:03 +0530

    Create freecourse.php

---
+++ freecourse.php
@@ -0,0 +1,123 @@
+<? php
+ session_start();
+ ?>
+<!DOCTYPE html>
+<html lang="en" >
+
+<head>
+  <meta charset="UTF-8">
+  <title>CSS3 pricing table</title>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js" type="text/javascript"></script>
+
+
+  
+  
+      <link rel="stylesheet" href="css/style.css">
+
+  
+</head>
+<style type="text/css">
+	img{
+		border-radius: 50%;
+	}
+</style>
+
+<body>
+ <h3 align="right"><font color="#ffffff"><a href="home.php">BACK TO HOME</a></font></h3>
+  <div id="pricing-table" class="clear">
+    <div class="plan">
+        <h3>Udemy<span><img class="img1" src="images/udemy.png" height="100" width="100"></span></h3>
+        <a class="signup" href="https://in.udemy.com/">Explore</a>         
+        <ul>
+            <li><b>10GB</b> Disk Space</li>
+            <li><b>100GB</b> Monthly Bandwidth</li>
+            <li><b>20</b> Email Accounts</li>
+			<li><b>Unlimited</b> subdomains</li>			
+        </ul> 
+    </div>
+    <div class="plan" id="most-popular">
+        <h3>Udacity<span><img src="images/udacity.png" height="100" width="100"></span></h3>
+        <a class="signup" href="https://www.udacity.com/">Explore</a>        
+        <ul>
+            <li><b>5GB</b> Disk Space</li>
+            <li><b>50GB</b> Monthly Bandwidth</li>
+            <li><b>10</b> Email Accounts</li>
+			<li><b>Unlimited</b> subdomains</li>			
+        </ul>    
+    </div>
+    <div class="plan">
+        <h3>Edx<span><img src="edx.png" height="100" width="100"></span></h3>
+		<a class="signup" href="https://www.edx.org">Explore</a>
+        <ul>
+            <li><b>3GB</b> Disk Space</li>
+            <li><b>25GB</b> Monthly Bandwidth</li>
+            <li><b>5</b> Email Accounts</li>
+			<li><b>Unlimited</b> subdomains</li>			
+        </ul>
+    </div>
+    <div class="plan">
+        <h3>Sololearn<span><img src="images/sololearn.png" height="90" width="90"></span></h3>
+        <a class="signup" href="https://www.sololearn.com/">Explore</a>		
+        <ul>
+            <li><b>1GB</b> Disk Space</li>
+            <li><b>10GB</b> Monthly Bandwidth</li>
+            <li><b>2</b> Email Accounts</li>
+			<li><b>Unlimited</b> subdomains</li>			
+        </ul>
+    </div> 	
+</div>
+  
+  
+  <div id="pricing-table" class="clear">
+    <div class="plan">
+        <h3>Openculture<span><img class="img1" src="images/oc.png" height="100" width="100"></span></h3>
+        <a class="signup" href="http://www.openculture.com/freeonlinecourses">Explore</a>         
+        <ul>
+            <li><b>10GB</b> Disk Space</li>
+            <li><b>100GB</b> Monthly Bandwidth</li>
+            <li><b>20</b> Email Accounts</li>
+            <li><b>Unlimited</b> subdomains</li>            
+        </ul> 
+    </div>
+    <div class="plan" id="most-popular">
+        <h3>NPTEL<span><img src="images/nptel.jpg" height="100" width="100"></span></h3>
+        <a class="signup" href="https://onlinecourses.nptel.ac.in/">Explore</a>        
+        <ul>
+            <li><b>5GB</b> Disk Space</li>
+            <li><b>50GB</b> Monthly Bandwidth</li>
+            <li><b>10</b> Email Accounts</li>
+            <li><b>Unlimited</b> subdomains</li>            
+        </ul>    
+    </div>
+    <div class="plan">
+        <h3>Edx<span><img src="images/coursera.png" height="100" width="100"></span></h3>
+        <a class="signup" href="https://www.coursera.org">Explore</a>
+        <ul>
+            <li><b>3GB</b> Disk Space</li>
+            <li><b>25GB</b> Monthly Bandwidth</li>
+            <li><b>5</b> Email Accounts</li>
+            <li><b>Unlimited</b> subdomains</li>            
+        </ul>
+    </div>
+    <div class="plan">
+        <h3>Sololearn<span><img src="images/sololearn.png" height="90" width="90"></span></h3>
+        <a class="signup" href="https://www.sololearn.com/">Explore</a>     
+        <ul>
+            <li><b>1GB</b> Disk Space</li>
+            <li><b>10GB</b> Monthly Bandwidth</li>
+            <li><b>2</b> Email Accounts</li>
+            <li><b>Unlimited</b> subdomains</li>            
+        </ul>
+    </div>  
+</div>
+  
+  
+
+    <script  src="js/index.js"></script>
+
+
+
+
+</body>
+
+</html>
