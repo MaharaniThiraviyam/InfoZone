a8415f04929c6a0a61e27606cf8c33d709ead6c7
T.MAHARANI <maharani1118@gmail.com>
2018-10-20 14:58:33 +0530

    Create internship.php

---
+++ internship.php
@@ -0,0 +1,111 @@
+<? php
+ session_start();
+ ?>
+
+<!DOCTYPE html>
+<html lang="en" >
+
+<head>
+  <meta charset="UTF-8">
+  <title>Search Intership</title>
+  
+  
+  
+      <link rel="stylesheet" href="css/styleintership.css">
+
+  <style type="text/css">
+    img{
+      border-radius: 50%;
+    }
+  </style>
+</head>
+
+<body>
+
+
+  <h3 align="right"><font color="#ffffff"><a href="home.php">BACK TO HOME</a></font></h3>
+  <h1 class="title"> INTERSHIP INFO </h1>
+  <div id="container">
+    <div class="pricetab">
+      <h1> INTERSHALA </h1>
+      <div class="price"> 
+        <img src="images/intershala.png" width="120" height="120">
+      </div>
+      <div class="infos">
+        <h3> Create Profile Now !!! </h3>
+         </div>
+      <div class="pricefooter">
+        <div class="button">
+          <a href="https://www.intershala.com"> Get started </a>
+        </div>
+      </div>
+    </div>
+    <div class="pricetab">
+      <h1> INTERSHIPS.COM </h1>
+      <div class="price"> 
+         <img src="images/interships.jpg" width="120" height="120">
+     
+      </div>
+      <div class="infos">
+        <h3> Create Profile Now !!! </h3>
+        
+      </div>
+      <div class="pricefooter">
+        <div class="button">
+          <a href="https://www.interships.com"> Get started </a>
+        </div>
+      </div>
+    </div>
+    <div class="pricetabmid">
+      <h1>INDEED</h1>
+      <div class="pricemid"> 
+        <img src="images/indeed.png" width="120" height="120">
+     
+      </div>
+      <div class="infos">
+        <h3> Start Your Career Now !!! </h3>
+      </div>
+      <div class="pricefootermid">
+        <div class="buttonmid">
+          <a href="https://www.indeed.com"> Get started </a>
+        </div>
+      </div>
+    </div>
+    <div class="pricetab">
+      <h1> LOOKSHARP</h1>
+      <div class="price"> 
+        <img src="images/looksharp.jpg" width="120" height="120">
+           </div>
+      <div class="infos">
+        <h3> Create Profile Now !!! </h3>
+       
+      </div>
+      <div class="pricefooter">
+        <div class="button">
+          <a href="https://www.looksharp.com"> Get started </a>
+        </div>
+      </div>
+    </div>
+    <div class="pricetab">
+      <h1>IDEALIST </h1>
+      <div class="price"> 
+        <img src="images/idealist.jpg" width="120" height="120">
+     
+      </div>
+      <div class="infos">
+        <h3> Create Profile Now !!! </h3>
+        </div>
+      <div class="pricefooter">
+        <div class="button">
+          <a href="https://www.idealist.com"> Get started </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+  
+  
+
+</body>
+
+</html>
