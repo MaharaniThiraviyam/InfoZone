e0472ab17a8632802696a604205e7b0a797f2210
T.MAHARANI <maharani1118@gmail.com>
2018-10-20 15:07:26 +0530

    Create study.php

---
+++ study.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en" >
+
+<head>
+  <meta charset="UTF-8">
+  <title>Pricing Table</title>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+
+  
+      <link rel="stylesheet" href="css/stylestudy.css">
+
+  
+</head>
+
+<body>
+	<h3 align="right"><font color="#ffffff"><a href="home.php">BACK TO HOME</a></font></h3>
+ 
+
+  	<div class="container group">
+		<div class="grid-1-5">
+			<h2>HACKER RANK</h2>
+			<h3><sup>Start Coding Now !!!</sup></h3>
+			<img src="images/hr.png" width="150" height="150">
+			<a href="https://www.hackerrank.com" class="button">EXPLORE</a>
+		</div>
+
+		<div class="grid-1-5">
+			<h2>GEEKS FOR GEEKS</h2>
+			<h3><sup>Coding is Easy !!!</sup></h3>
+				  <img src="images/gg.png" width="150" height="150">
+    
+			<a href="https://www.geeksforgeeks.org" class="button">EXPLORE</a>	
+		</div>
+		<div class="grid-1-5">
+			<h2>TOP CODERS</h2>
+			<h3><sup>Do Coding Now !!! </sup></h3>
+		  <img src="images/topcoder.jpg" width="150" height="150">
+    
+			<a href="https://www.topcoder.com" class="button">Explore</a>		
+		</div>
+		<div class="grid-1-5">
+			<h2>HACKER EARTH</h2>
+			<h3><sup>Coding is Joyful !!!</sup></h3>
+			 <img src="images/hr.png" width="150" height="150">
+    
+			<a href="https://www.hackerearth.com" class="button">EXPLORE</a>		
+		</div>
+		<div class="grid-1-5">
+			<h2>TECHGIG</h2>
+			<h3><sup>Start Explore Now !!!</sup></h3>
+			 <img src="images/techgig.png" width="150" height="150">
+    
+			<a href="https://www.techgig.com" class="button">EXPORE</a>
+		</div>		
+	</div>
+  
+  
+
+</body>
+
+</html>
